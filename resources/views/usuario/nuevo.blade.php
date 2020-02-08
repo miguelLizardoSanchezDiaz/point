3996849496985983113492126b3e2d045756634d
Miguel Lizardo <shagui2607@gmail.com>
2020-02-08 12:44:25 -0500

    ajustes vista nuevo usuario

--- resources/views/usuario/nuevo.blade.php
+++ resources/views/usuario/nuevo.blade.php
@@ -38,8 +38,44 @@
                         <a href="{{url($variable)}}" class="btn btn-sm btn-danger"><span class="fas fa-chevron-left"></span> Atras</a>
                     </div>
                     <div class="col-sm-6 col-xs-12">
-                        @include('mensajes.successful')
+                        @include('errors.errores')
                     </div>
+
+                    <form class="col-sm-12 col-xs-12" method="POST" action="{{url($variable)}}" accept-charset="UTF-8" enctype="multipart/form-data">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <fieldset>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Usuario (*)</label>
+                                        {!! Form::text('email',null,['class' => 'form-control','id'=>'email', 'maxlength'=>'250','placeholder'=>'Ingrese usuario']) !!}
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Nombres (*)</label>
+                                        {!! Form::text('txt_nombre',null,['class' => 'form-control','id'=>'txt_nombre', 'maxlength'=>'250','placeholder'=>'Ingrese nombre']) !!}
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Rol</label>
+                                        <select class="form-control" id="cbo_rol" name="cbo_rol">
+                                            @foreach($roles as $rol)
+                                                <option value="{{$rol->id}}">{{$rol->rol_descripcion}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Contraseña (*)</label>
+                                        <input type="password" class="form-control" name="txt_contraseña1" id="txt_contraseña1" maxlength="25" placeholder="****************">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Repita Contraseña (*)</label>
+                                        <input type="password" class="form-control" name="txt_contraseña2" id="txt_contraseña2" maxlength="25" placeholder="****************">
+                                    </div>
+                                    <div class="form-group">
+                                    <label>(*) Campos Obligatorios</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-sm btn-primary m-r-5"><span class="glyphicon glyphicon-save"></span> Registrar</button>
+                                    <a href="{{url($variable)}}" class="btn btn-sm btn-danger"><span class="glyphicon glyphicon-remove"></span> Cancelar</a>
+                                </fieldset>
+                            </form>
+
                 </div>
                
               </div>
