9d7eb05ee893fbe17daa11b87253498937fd1a68
Miguel Lizardo <shagui2607@gmail.com>
2020-03-02 17:10:10 -0500

    nuevo productos

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,7 +2,9 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{url('home')}}" class="brand-link">
-      <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      
+      {!! HTML::image('/adminlte/dist/img/AdminLTELogo.png','AdminLTE Logo',array('class'=>'brand-image img-circle elevation-3','style'=>'opacity: .8')) !!}
+
       <span class="brand-text font-weight-light">Medida Perú</span>
     </a>
 
@@ -11,7 +13,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          {!! HTML::image('/adminlte/dist/img/user2-160x160.jpg','User Image',array('class'=>'img-circle elevation-2')) !!}
         </div>
         <div class="info">
           <a href="{{url('home')}}" class="d-block">{{ Auth::user()->name }}</a>
