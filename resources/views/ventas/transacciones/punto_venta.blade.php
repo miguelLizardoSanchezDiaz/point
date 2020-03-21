c5aa4114eec2a8b29a27f4e0d9d44803d712faaf
Miguel Lizardo <shagui2607@gmail.com>
2020-03-21 11:49:01 -0500

    maqueta punto venta 2

--- resources/views/ventas/transacciones/punto_venta.blade.php
+++ resources/views/ventas/transacciones/punto_venta.blade.php
@@ -21,13 +21,194 @@
               <div class="card-body">
                 <div class="row">
                 <div class="col-sm-8 col-xs-12">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <div class="row">
+                  
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                  <div class="color_negro">
+                    <div class="row padding_descripcion">
+                      Detergente sapolio en botella
+                    </div>
+                    <div class="row fondo_1 color_blanco alinear_derecha">
+                      S/ 100.00
+                    </div>
+                  </div>
+                  </a>
+                  
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                  <div class="color_negro">
+                    <div class="row">
+                      Detergente sapolio x 1lt 2131 123 123
+                    </div>
+                    <div class="row">
+                      S/ 100.00
+                    </div>
+                  </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+                  <a href="#" class="col-sm-2 col-xs-12 color_negro sin_decoration_a altura_producto borde_producto efecto_hover fondo_blanco">
+                    <div class="color_negro">
+                      <div class="row">
+                        Detergente sapolio
+                      </div>
+                      <div class="row">
+                        S/ 100.00
+                      </div>
+                    </div>
+                  </a>
+
+
+                  </div>
                 </div>
+
+
                 <div class="col-sm-4 col-xs-12">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
